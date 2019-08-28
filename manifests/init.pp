eadb3752b07de943b7346a516238f7bd6ee8de9f
jordiprats <jordi.prats@gmail.com>
2019-08-28 19:03:33 +0200

    cleanup

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,11 @@
-class grafana(
-                            $manage_package        = true,
-                            $package_ensure        = 'installed',
-                            $manage_service        = true,
-                            $manage_docker_service = true,
-                            $service_ensure        = 'running',
-                            $service_enable        = true,
-                          ) inherits grafana::params{
-
-  validate_re($package_ensure, [ '^present$', '^installed$', '^absent$', '^purged$', '^held$', '^latest$' ], 'Not a supported package_ensure: present/absent/purged/held/latest')
+class grafana (
+                $manage_package        = true,
+                $package_ensure        = 'installed',
+                $manage_service        = true,
+                $manage_docker_service = true,
+                $service_ensure        = 'running',
+                $service_enable        = true,
+              ) inherits grafana::params{
 
   class { '::grafana::install': }
   -> class { '::grafana::config': }
--- manifests/service.pp
+++ manifests/service.pp
@@ -1,12 +1,5 @@
 class grafana::service inherits grafana {
 
-  #
-  validate_bool($grafana::manage_docker_service)
-  validate_bool($grafana::manage_service)
-  validate_bool($grafana::service_enable)
-
-  validate_re($grafana::service_ensure, [ '^running$', '^stopped$' ], "Not a valid daemon status: ${grafana::service_ensure}")
-
   $is_docker_container_var=getvar('::eyp_docker_iscontainer')
   $is_docker_container=str2bool($is_docker_container_var)
 
